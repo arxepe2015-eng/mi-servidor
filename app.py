80624265a84ff95db212224fc947354023d15268
arxepe2015-eng <arxepe.2015@gmail.com>
2026-09-01 10:53:36 +0200

    Add async_mode to Socket.IO initialization

--- app.py
+++ app.py
@@ -5,18 +5,15 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'arxechat_clave_secreta_123'
 
-# Inicializamos Socket.IO para el tiempo real
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
 @app.route('/')
 def home():
     return "Servidor de Arxechat activo y listo para WebSockets"
 
-# Escuchar cuando un usuario envía un mensaje
 @socketio.on('mensaje_enviado')
 def manejar_mensaje(data):
     print("Mensaje recibido:", data)
-    # Reenviar el mensaje a todos los usuarios conectados
     emit('recibir_mensaje', data, broadcast=True)
 
 if __name__ == '__main__':
