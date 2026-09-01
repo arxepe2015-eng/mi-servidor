bb813d2cfb45862081013b428449717086c2e99d
arxepe2015-eng <arxepe.2015@gmail.com>
2026-09-01 11:00:32 +0200

    Enable allow_unsafe_werkzeug for socketio.run

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def manejar_mensaje(data):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
